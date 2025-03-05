bb728229a7c23ef25bce8011dbf6cde708bd70f7
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2025-03-05 19:07:57 +0200

    cpp repo - initial upload

---
+++ 00.Introduction/HelloWorld.cpp
@@ -0,0 +1,17 @@
+//
+// Created by mokgul on 3/5/25.
+//
+
+#include "HelloWorld.h"
+
+#include <iostream>
+
+HelloWorld::HelloWorld()
+{
+
+}
+
+void HelloWorld::sayHello()
+{
+    std::cout << "Hello World!";
+}
---
+++ 00.Introduction/HelloWorld.h
@@ -0,0 +1,16 @@
+//
+// Created by mokgul on 3/5/25.
+//
+
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
+
+class HelloWorld {
+public:
+    HelloWorld();
+    void sayHello();
+};
+
+#endif //HELLOWORLD_H
